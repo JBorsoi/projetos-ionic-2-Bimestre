68a3a0dfbe272c3a1e4a18c247cf4c84c933a1a8
João Pedro Borsoi <105085062+JBorsoi@users.noreply.github.com>
2022-08-16 20:57:36 -0300

    update

--- lista de compras/src/app/home/home.page.ts
+++ lista de compras/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
@@ -7,22 +8,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private storage:Storage) {
+    this.storage.create();
+  }
 
   variavel_lista_nome = [];
   variavel_lista_preco = [];
-  variavel_total: number;
   texto: string = "";
   preco: number;
+  aux;
 
-  adiciona() {
+  async adiciona() {
     if (!(this.texto == "")) {
     this.variavel_lista_nome.push(this.texto);
     this.variavel_lista_preco.push(this.preco);
+    this.variavel_lista_nome.forEach(item => {
+      if(parseInt(item[0]) > this.aux) {
+        this.aux = parseInt(item[0]);
+      }
+    })
+    this.aux = this.aux + 1
+    await this.storage.set(this.aux.toString(),this.texto);
+    this.atualizaLista();
     this.texto = "";
     this.preco = 0;
-    this.variavel
+
     }
+  }
+
+  atualizaLista() {
+    this.variavel_lista_nome = [];
+    this.storage.forEach((value, key, index) => {
+      this.variavel_lista_nome.push([key, value]);
+    })
   }
 
   remove(indice) {
